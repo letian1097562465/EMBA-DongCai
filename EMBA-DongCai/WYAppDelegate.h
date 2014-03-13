cbc8b1006f11eceeedfb8936a3fdb44efed01c31
wanyue <apple@appletekiMacBook-Pro.local>
2014-03-13 13:17:08 +0800

    Initial Commit

---
+++ EMBA-DongCai/WYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WYAppDelegate.h
+//  EMBA-DongCai
+//
+//  Created by wanyue on 14-3-13.
+//  Copyright (c) 2014年 wanyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EMBA-DongCai/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EMBA-DongCai
+//
+//  Created by wanyue on 14-3-13.
+//  Copyright (c) 2014年 wanyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WYAppDelegate class]));
+    }
+}
